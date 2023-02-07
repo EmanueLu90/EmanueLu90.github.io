f904e2a0fb8fb6f82382579d4bfb1e97b33063ec
ema_fraile@hotmail.com <ema_fraile@hotmail.com>
2023-02-06 23:15:22 -0300

    integramos a modo prueba fetch a js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+//fetch('https://jsonplaceholder.typicode.com/todos/1')
+fetch('https://randomuser.me/api/')
+  .then(response => response.json())
+  .then(json => console.log(json))
 
-  
-
+/**document.getElementById('botón').onclick=function(){console.log("Capturamos el evento click");
+  document.getElementById("demo").innerHTML="PROBAMOS NUESTRO PRIMER EVENTO EN HTML"
+}
+*/
