40ff9fa9d48fe5d730616eaa5f550079c382fd8c
ema_fraile@hotmail.com <ema_fraile@hotmail.com>
2023-02-12 10:03:03 -0300

    agregado de datos personales

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //fetch('https://jsonplaceholder.typicode.com/todos/1')
 fetch('https://randomuser.me/api/')
-  .then(response => response.json())
-  .then(json => console.log(json))
-
+  .then(response => response.json(/**[0].name.first*/))
+  .then(json => console.log(json));
+  
 /**document.getElementById('botón').onclick=function(){console.log("Capturamos el evento click");
   document.getElementById("demo").innerHTML="PROBAMOS NUESTRO PRIMER EVENTO EN HTML"
 }
