79d9173ef90c703fcebbbb6e09299d4b1329f944
ema_fraile@hotmail.com <ema_fraile@hotmail.com>
2023-02-15 11:50:19 -0300

    agregue cambios en la fuente y botones

--- main.js
+++ main.js
@@ -2,8 +2,39 @@
 fetch('https://randomuser.me/api/')
   .then(response => response.json(/**[0].name.first*/))
   .then(json => console.log(json));
-  
-/**document.getElementById('botón').onclick=function(){console.log("Capturamos el evento click");
-  document.getElementById("demo").innerHTML="PROBAMOS NUESTRO PRIMER EVENTO EN HTML"
+/***/
+console.log("hola mundo");
+
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+	.then((response) => response.json())
+	.then((json) => console.log(json));
+
+document.getElementById('boton').onclick = function () {
+	console.log("capturamos el evento click");
+	document.getElementById('demo').innerHTML = "estamos probando nuestro evento js";
 }
-*/
+
+
+
+document.addEventListener('click', function() {
+		console.log("hola mundo 2");
+		document.getElementById("demo").innerHTML = "cambia msj";
+//generico afecta a todo el documento cuando hace click
+});
+
+
+
+document.getElementById('boton').addEventListener('click', function() {
+		console.log("hola mundo 2");
+		document.getElementById("demo").innerHTML = "cambia msj";
+});
+
+
+
+
+
+document.getElementById('boton_color').addEventListener('click', function () {
+	document.body.style.backgroundColor = '#FF0000';
+});
+
+/***/
