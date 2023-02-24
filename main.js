f08ef3780d805cdf1d8e97b2ffd15bcf234efefa
ema_fraile@hotmail.com <ema_fraile@hotmail.com>
2023-02-24 01:48:13 -0300

    cambio tamaños en letras e iconos

--- main.js
+++ main.js
@@ -5,19 +5,26 @@ fetch('https:randomuser.me/api/?exc=login')
       .then(data => mostrarData(data))
       .catch(error => console.log(error))
 
+
+
+
 const mostrarData = (data) => {
     console.log(data);
     let obj = data.results[0];
-    document.getElementById("nombre").innerHTML ='<i class="fa fa-user-circle-o" aria-hidden="true"></i> Nombre y Apellido: '+obj.name.first+', '+obj.name.last+'.';
-    document.getElementById("hombreMujer").innerHTML = '<i class="fa fa-venus-mars" aria-hidden="true"></i> Genero: '+obj.gender+'.';
-    document.getElementById("edad").innerHTML ='<i class="fa fa-heartbeat" aria-hidden="true"></i> Edad: '+obj.dob.age+' años.';
-    document.getElementById("fechaNacimiento").innerHTML = '<i class="fa fa-birthday-cake" aria-hidden="true"></i> Fecha de Nacimiento: '+obj.dob.date+'.';
-    document.getElementById("direccion").innerHTML = '<i class="fa fa-home" aria-hidden="true"></i> Direccion: '+obj.location.street.name+', Numero: '+obj.location.street.number+'.';
-    document.getElementById("ciudad").innerHTML ='<i class="fa fa-flag" aria-hidden="true"></i> Ciudad: '+obj.location.city+', '+obj.location.state+', '+obj.location.country+'.';
-    document.getElementById("telefono").innerHTML = '<i class="fa fa-phone" aria-hidden="true"></i> Telefono Celular: '+obj.cell+'.';
-    document.getElementById("email").innerHTML = '<i class="fa fa-at" aria-hidden="true"></i> Email: '+obj.email+'.';
+    
+    document.getElementById("nombre").innerHTML ='<i class="fa fa-user-circle-o fa-lg" aria-hidden="true"></i> Nombre y Apellido: '+obj.name.first+', '+obj.name.last+'.';
+    document.getElementById("hombreMujer").innerHTML = '<i class="fa fa-venus-mars fa-lg" aria-hidden="true"></i> Genero: '+obj.gender+'.';
+    document.getElementById("edad").innerHTML ='<i class="fa fa-heartbeat fa-lg" aria-hidden="true"></i> Edad: '+obj.dob.age+' años.';
+    document.getElementById("fechaNacimiento").innerHTML = '<i class="fa fa-birthday-cake fa-lg" aria-hidden="true"></i> Fecha de Nacimiento: '+obj.dob.date+'.';
+    document.getElementById("direccion").innerHTML = '<i class="fa fa-home fa-lg" aria-hidden="true"></i> Direccion: '+obj.location.street.name+', Numero: '+obj.location.street.number+'.';
+    document.getElementById("ciudad").innerHTML ='<i class="fa fa-flag fa-lg" aria-hidden="true"></i> Ciudad: '+obj.location.city+', '+obj.location.state+', '+obj.location.country+'.';
+    document.getElementById("telefono").innerHTML = '<i class="fa fa-phone fa-lg" aria-hidden="true"></i> Telefono Celular: '+obj.cell+'.';
+    document.getElementById("email").innerHTML = '<i class="fa fa-at fa-lg" aria-hidden="true"></i> Email: '+obj.email+'.';
     document.getElementById("fotografia").innerHTML = '<img src='+obj.picture.large+' alt=foto de perfil>';
 }
+
+if (hombreMujer='male'){
+document.getElementById("hombreMujer").innerHTML = document.body.seccion.style.backgroundColor= '#FF0000' ;}
 
 
 
