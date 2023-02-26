6ba0624f348089685a9a092e54cd898691230043
ema_fraile@hotmail.com <ema_fraile@hotmail.com>
2023-02-26 01:33:59 -0300

    quitar errores

--- main.js
+++ main.js
@@ -35,8 +35,8 @@ mostrar('https:randomuser.me/api/?exc=login');
 /*por medio del boton perfil random recarga el perfil por uno nuevo*/
 document.getElementById('boton_api').addEventListener('click', function() {
   mostrar('https:randomuser.me/api/?exc=login');
-  document.body.style.backgroundColor= 'blue';
-  document.nombre.style.display='none';
+  /*document.body.style.backgroundColor= 'blue';*/
+  document.nombre.display=none;
 });
 
 /*Json con datos del desarrollador*/
@@ -61,8 +61,8 @@ function mostrarData2(data2){
 /*Funcion del boton que oculta el contenido del curriculum random de la api por la del desarrollador del proyecto*/
 document.getElementById('boton_admin').addEventListener('click', function() {
   mostrarData2(datosDesarrollador);
-  document.body.style.backgroundColor = 'red';
-  document.nombre.style.display='none';
+  /*document.body.style.backgroundColor = 'red';*/
+  document.nombre.display=none;
 });
 
 
