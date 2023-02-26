73b6c7d4f15ae89dcb3bed8ce215c8ad3468c3c5
ema_fraile@hotmail.com <ema_fraile@hotmail.com>
2023-02-25 21:54:12 -0300

    botones perfiles completos

--- main.js
+++ main.js
@@ -29,15 +29,19 @@ fetch(url)
     document.getElementById("fotografia").innerHTML = '<img src='+obj.picture.large+' alt=foto de perfil>';
 }}
 
-
-document.getElementById('boton_api').onclick =
+/*inicializo la funcion mostrar cv random de la api*/
 mostrar('https:randomuser.me/api/?exc=login');
 
+/*por medio del boton perfil random recarga el perfil por uno nuevo*/
+document.getElementById('boton_api').addEventListener('click', function() {
+  mostrar('https:randomuser.me/api/?exc=login');
+  document.body.style.backgroundColor = '';
+  document.nombre.style.display = 'none';
+});
 
-/*mostrar('https://randomuser.me/api/?seed=3f4390305d2e7e22');*/
+/*Json con datos del desarrollador*/
 
-
-const datosDesarrollador = {"results":[{"gender":"Masculino","name":{"first":"Emanuel","last":"Fraile"},"location":{"street":{"number":7756,"name":"Fantasia"},"city":"Rio Cuarto","state":"Cordoba","country":"Argentina"},"email":"emanuel.f@example.com","dob":{"date":"1990-05-31","age":32},"cell":"(557)-261-7416","picture":{"large":"https://randomuser.me/api/portraits/men/15.jpg"}}]}
+const datosDesarrollador = {"results":[{"gender":"Masculino","name":{"first":"Emanuel","last":"Fraile"},"location":{"street":{"number":7756,"name":"Fantasia"},"city":"Río Cuarto","state":"Córdoba","country":"Argentina"},"email":"emanuel.f@example.com","dob":{"date":"1990-05-31","age":32},"cell":"(557)-261-7416","picture":{"large":"https://randomuser.me/api/portraits/men/15.jpg"}}]}
 JSON.stringify(datosDesarrollador);
 
 function mostrarData2(data2){
@@ -54,17 +58,13 @@ function mostrarData2(data2){
     document.getElementById("fotografia").innerHTML = '<img src=' + obj.picture.large + ' alt=foto de perfil>';
 }
 
-document.getElementById('boton_admin').onclick =
-mostrar(' ');
-mostrarData2(datosDesarrollador)
-;
-
-
-
-/*
+/*Funcion del boton que oculta el contenido del curriculum random de la api por la del desarrollador del proyecto*/
 document.getElementById('boton_admin').addEventListener('click', function() {
-      document.getElementById("email").innerHTML = "cambia msj";
-}*/
+  mostrarData2(datosDesarrollador);
+  document.body.style.backgroundColor = '';
+  document.nombre.style.display='none';
+});
+
 
 
 /* funcion pestaña ejemplo acordeon sacado de w3schools*/
